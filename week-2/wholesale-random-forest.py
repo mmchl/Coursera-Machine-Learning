7206240eebf5a9db4eb7fb031c1fe49d38db0d52
mmchl <mmchl@users.noreply.github.com>
2018-01-25 16:29:41 +0300

    Add files via upload

---
+++ week-2/wholesale-random-forest.py
@@ -0,0 +1,71 @@
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pylab as plt
+from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+import sklearn.metrics
+ # Feature Importance
+from sklearn import datasets
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+#Load the dataset
+
+dataset = pd.read_csv('wholesale.csv')
+data_clean = dataset.dropna()
+
+data_clean.dtypes
+data_clean.describe()
+
+#Split into training and testing sets
+
+predictors = data_clean[['Channel', 'Fresh', 'Milk', 'Grocery', 'Frozen',
+                         'Detergents_Paper', 'Delicassen']]
+
+targets = data_clean.Region
+
+pred_train, pred_test, tar_train, tar_test  = train_test_split(predictors, targets, test_size=.4)
+
+pred_train.shape
+pred_test.shape
+tar_train.shape
+tar_test.shape
+
+#Build model on training data
+
+classifier=RandomForestClassifier(n_estimators=25)
+classifier=classifier.fit(pred_train,tar_train)
+
+predictions=classifier.predict(pred_test)
+
+sklearn.metrics.confusion_matrix(tar_test,predictions)
+sklearn.metrics.accuracy_score(tar_test, predictions)
+
+
+# fit an Extra Trees model to the data
+model = ExtraTreesClassifier()
+model.fit(pred_train,tar_train)
+# display the relative importance of each attribute
+print(model.feature_importances_)
+
+
+"""
+Running a different number of trees and see the effect
+ of that on the accuracy of the prediction
+"""
+
+trees=range(25)
+accuracy=np.zeros(25)
+
+for idx in range(len(trees)):
+   classifier=RandomForestClassifier(n_estimators=idx + 1)
+   classifier=classifier.fit(pred_train,tar_train)
+   predictions=classifier.predict(pred_test)
+   accuracy[idx]=sklearn.metrics.accuracy_score(tar_test, predictions)
+   
+plt.cla()
+plt.plot(trees, accuracy)
+
